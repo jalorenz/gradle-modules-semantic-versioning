c7dd03fd997fdc88f3d565792c7e34bc5f987fcc
Jannik Lorenz <jannik.lorenz@otto.de>
2026-05-04 09:14:16 +0200

    chore: adds semantic versioning action

---
+++ lib-b/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "de.jalorenz"
+            artifactId = "lib-b"
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/jalorenz/gradle-modules-semantic-versioning")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+}
