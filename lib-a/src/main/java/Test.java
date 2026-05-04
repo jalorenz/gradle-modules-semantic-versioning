8a2f3cb7a2e8c5cf835e0bbefe5e42315d578a0a
Jannik Lorenz <jannik.lorenz@otto.de>
2026-05-04 14:56:20 +0200

    feat!: removes most of the funcs

--- lib-a/src/main/java/Test.java
+++ lib-a/src/main/java/Test.java
@@ -6,16 +6,4 @@ class Test {
     static void test3 () {
         System.out.println("lib-b test3");
     }
-
-    void helloWorld() {
-        System.out.println("lib-b test3");
-    }
-
-    void helloWorld2() {
-        System.out.println("lib-b test3");
-    }
-
-    void helloWorld3() {
-        System.out.println("lib-b test3");
-    }
 }
